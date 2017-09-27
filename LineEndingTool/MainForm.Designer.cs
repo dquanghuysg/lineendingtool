cd9ed4baf6b402e2e9740acb784d6f884172ec69
Huy Dam Quang 1 <huy.damquang1@nashtechglobal.com>
2017-09-27 11:23:10 +0700

    Initialized project

---
+++ LineEndingTool/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace LineEndingTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFileFormat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.listMessage = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtFileFormat);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnFind);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtFolder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(772, 76);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtFileFormat
+            // 
+            this.txtFileFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileFormat.Location = new System.Drawing.Point(102, 42);
+            this.txtFileFormat.Name = "txtFileFormat";
+            this.txtFileFormat.Size = new System.Drawing.Size(577, 24);
+            this.txtFileFormat.TabIndex = 4;
+            this.txtFileFormat.Text = "*.cs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File Format";
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.Location = new System.Drawing.Point(685, 6);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 32);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolder.Location = new System.Drawing.Point(69, 10);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(610, 24);
+            this.txtFolder.TabIndex = 0;
+            this.txtFolder.Text = "D:\\05_project\\12_GroveDean\\02_sources\\quoteengine\\WebQuoteApp\\Areas\\ModernMVC\\Hel" +
+    "pers";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 384);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(772, 39);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(685, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 32);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // listMessage
+            // 
+            this.listMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listMessage.FormattingEnabled = true;
+            this.listMessage.ItemHeight = 18;
+            this.listMessage.Location = new System.Drawing.Point(0, 76);
+            this.listMessage.Name = "listMessage";
+            this.listMessage.Size = new System.Drawing.Size(772, 308);
+            this.listMessage.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(685, 38);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 32);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 423);
+            this.Controls.Add(this.listMessage);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Line Ending Tool";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtFileFormat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listMessage;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ LineEndingTool/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LineEndingTool
+{
+    public partial class MainForm : Form
+    {
+        delegate void SetTextCallback(string _text);
+
+        private StringBuilder messages = new StringBuilder();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowMessage(string _text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.listMessage.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(ShowMessage);
+                this.Invoke(d, new object[] { _text });
+            }
+            else
+            {
+                messages.Append(_text);
+                this.listMessage.Items.Add(_text);
+                this.listMessage.Update();
+                //this.rtxtMessage.Text = messages.ToString();
+                //this.rtxtMessage.Update();
+            }
+        }
+
+        private int total = 0;
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            ShowMessage($"Start Find: {this.txtFolder.Text}\\{this.txtFileFormat.Text}\r\n");
+            this.procceed(this.txtFolder.Text, this.txtFileFormat.Text, false);
+            ShowMessage($"End {total}");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void procceed(string path, string fileFormats, bool replaced)
+        {            
+            //StringBuilder str = new StringBuilder();
+            DirectoryInfo d = new DirectoryInfo(path);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles(fileFormats); //Getting Text files
+            
+            foreach (FileInfo file in Files)
+            {
+                long count1 = this.Count(file.FullName, "\r");
+                long count2 = this.Count(file.FullName, "\n");
+                if (count1 != count2)
+                {
+                    total++;
+                    string str = $"{file.FullName} ({count1})({count2})\r\n";
+
+                    if (replaced)
+                    {
+                        this.Update(file.FullName);
+                    }
+                    ShowMessage(str);
+                }
+            }
+
+            DirectoryInfo[] dics = d.GetDirectories();
+            foreach (DirectoryInfo sd in dics)
+            {
+                procceed(sd.FullName, fileFormats, replaced);
+            }
+        }
+
+        private long Count(string filePath, string pattern)
+        {
+            string str = File.ReadAllText(filePath);
+
+            long count = str.Length - str.Replace(pattern, "").Length;
+
+            return count;
+        }
+
+        private void Update(string filePath)
+        {
+            string pattern1 = "\r";
+            string pattern2 = "\n";
+            string pattern3 = "\r\n";
+
+            string str = File.ReadAllText(filePath);
+
+            str = str.Replace(pattern3, pattern1);
+            str = str.Replace(pattern2, pattern1);
+            str = str.Replace(pattern1, pattern3);
+
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                {
+                    writer.Write(str);
+                }
+                writer.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            ShowMessage($"Start Update: {this.txtFolder.Text}\\{this.txtFileFormat.Text}\r\n");
+            this.procceed(this.txtFolder.Text, this.txtFileFormat.Text, true);
+            ShowMessage($"End {total}");
+        }
+    }
+}
